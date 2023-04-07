78525dcbca6b125cef90daff46cf640081a6e030
ServantofLord <128671036+ServantofLord@users.noreply.github.com>
2023-04-07 18:36:11 +0800

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Lawrence Lau on 7/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
